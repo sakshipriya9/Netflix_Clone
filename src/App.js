681271ffee204a72d1cf2f8605a02134e652297f
Sakshi Priya <priyasakshi8989@gmail.com>
2025-03-21 21:31:47 +0530

    created Banner.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Banner from './Components/Banner';
 import Navbar from './Components/Navbar';
 
 function App() {
   return(
     <div className='app'>
         <Navbar />
+        <Banner />
     </div>
   )
 };
