885068987319242931bc412c3b3412b0a0e8d531
Sakshi Priya <priyasakshi8989@gmail.com>
2025-03-25 13:35:33 +0530

    Row.js completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import React from 'react';
 import './App.css';
+import Row from './Components/Row';
+import requests from './requests';
 import Banner from './Components/Banner';
 import Navbar from './Components/Navbar';
 
 function App() {
-  return(
-    <div className='app'>
-        <Navbar />
-        <Banner />
-    </div>
-  )
-};
+    return (
+        <div className="app">
+            <Navbar />
+            <Banner />
+            <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
+            <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+            <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+            <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+            <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+            <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+            <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+        </div>
+    );
+}
 
 export default App;
